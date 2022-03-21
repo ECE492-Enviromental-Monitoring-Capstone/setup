100a81543493dbbcf41bdff627421c544935e86f
Jehanzeb Mirza <jehanzeb@ualberta.ca>
2022-03-21 12:44:16 -0600

    Updated Script To Download Files and Unzip them
    Next is DD and then formatting.

--- setup_sd_card.sh
+++ setup_sd_card.sh
@@ -10,17 +10,31 @@
 # BUT DOUBLE TRIPLE CHECK BEFORE YOU SCREW STUFF UP.
 # This script doesn't redownload files if they already exist.
 
-if [ -z "$1"]
+# Basic variables
+pi_os_url="https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2022-01-28/2022-01-28-raspios-bullseye-armhf-lite.zip"
+
+# Check to make sure user arguments are correct.
+if [ -z "$1" ]
+then
 	echo First argument should be a scratch directory
 	echo ex: ./temp/
 	exit 1
 fi
-
-if [ -z "$2"]
+if [ -z "$2" ]
+then
 	echo Second argument should be microSD card device
 	echo Double triple check which device it is.
 	echo Check using "sudo fdisk -l"
 	echo ex: /dev/sdc
 	exit 1
 fi
+
+echo "Making Directory"
+mkdir $1/
+
+echo "Fetching Pi OS"
+wget -nc -P "$1/" "${pi_os_url}"
+
+echo "Unzipping OS"
+unzip -n  -d "$1/" "${1}/*.zip"
 
